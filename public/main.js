504b76109d9e8e230bd77dd81cc3885c707b747a
Damon Kalaboukis <damonkala@yahoo.com>
2016-04-26 10:53:05 -0700

    First upd8 in a while

--- public/main.js
+++ public/main.js
@@ -1,14 +1,30 @@
+$(document).foundation();
+var reading = false;
 $(document).ready(function() {
 		console.log("Doc Ready")
     $('#fullpage').fullpage({
 			anchors: ['About', 'Skills', 'Projects', 'Experience'],
 		});
 		$('.subject').click(goTo);
+		$('.magicModal').click(disAble);
+		$('.close-button').click(enAble);
 });
 
 
 function goTo(e){
-	var page = e.target.text;
-	console.log(page)
-	$.fn.fullpage.moveTo(page);
+	if(!reading){
+		var page = e.target.text;
+		console.log(page)
+		$.fn.fullpage.moveTo(page);
+	}
+}
+function disAble(){
+	reading = true;
+	$.fn.fullpage.setMouseWheelScrolling(false);
+	$.fn.fullpage.setAllowScrolling(false);
+}
+function enAble(){
+	reading = false;
+	$.fn.fullpage.setMouseWheelScrolling(true);
+	$.fn.fullpage.setAllowScrolling(true);
 }
